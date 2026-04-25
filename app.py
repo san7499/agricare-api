c54ec7d2a15643b2cb450b54bfd138cddd758bd6
Sanket Khapake <157945367+san7499@users.noreply.github.com>
2026-04-25 21:14:40 +0530

    Change default port from 10000 to 7860

--- app.py
+++ app.py
@@ -267,5 +267,5 @@ def predict():
 
 # ------------------ RUN ------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 7860))
     app.run(host="0.0.0.0", port=port)
